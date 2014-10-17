3599cc40f6ca4ae09c5ed5d897776e0ba6498841
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-17 22:53:28 +0100

    Added Frame class

---
+++ spec/FrameSpec.js
@@ -0,0 +1,8 @@
+describe('Frame', function() {
+	it('should consist of a number of rolls', function() {
+		frame = new Frame;
+		frame.haha
+		expect(Object.keys(frame.rolls).length).toEqual(0);
+	});
+
+});
