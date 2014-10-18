18fcc00ec3969adae4bf9b4fc8c2c5cd64541785
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-18 15:17:40 +0100

    Frame knows if it is or isn't a spare

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -3,7 +3,27 @@ describe('Frame', function() {
 	beforeEach(function() {
 		frame = new Frame;
 		roll = new Roll;
+		pin = new Pin;
+		pin.hit();
 	});
+
+	makeSpare = function(frame) {
+		roll1 = new Roll; 
+			for(var i = 1; i < 6; i++){
+				pin = new Pin;
+				pin.hit();
+				roll1.addPin(pin);
+			};
+
+		roll2 = new Roll;
+			for(var i = 1; i < 6; i++){
+				pin = new Pin;
+				pin.hit();
+				roll2.addPin(pin);
+			};
+		frame.addRoll(roll1);
+		frame.addRoll(roll2);
+	};
 
 	it('should consist of a number of rolls', function() {
 		expect(frame.rolls.length).toEqual(0);
@@ -21,10 +41,28 @@ describe('Frame', function() {
 	});
 
 	it('knows the total score of all the rolls in this frame', function() {
-		pin = new Pin(2);
-		pin.hit();
+		pinDouble = new Pin(2);
+		pinDouble.hit();
+		roll.addPin(pin);
+		roll.addPin(pinDouble);
+		frame.addRoll(roll);
+		expect(frame.score()).toEqual(3);
+	});
+
+	it('total number of pins knocked down in this frame', function() {
 		roll.addPin(pin);
 		frame.addRoll(roll);
-		expect(frame.score()).toEqual(2);
+		expect(frame.totalPins()).toEqual(1);
+	});
+
+	it('knows if it is a spare', function() {
+		makeSpare(frame);
+		expect(frame.isSpare()).toBe(true);
+	})
+
+	it('knows if it is not a spare', function() {
+		roll.addPin(pin);
+		frame.addRoll(roll);
+		expect(frame.isSpare()).toBe(false);
 	});
 });
--- src/Frame.js
+++ src/Frame.js
@@ -15,6 +15,18 @@ Frame.prototype.score = function() {
 	return total;
 };
 
+Frame.prototype.totalPins = function() {
+	var total = 0;
+	for(var i = 0; i < this.rolls.length; i++) {
+		total = total + this._selectRoll(i).pins.length;
+	};
+	return total;
+};
+
 Frame.prototype._selectRoll = function(number) {
 	return this.rolls[number];
+};
+
+Frame.prototype.isSpare = function() {
+	return (this.totalPins() === 10);
 };
